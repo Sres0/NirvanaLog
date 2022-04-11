b2392931e0b65e57ba537e47919f7391f4685797
Sres0 <susanarepo@gmail.com>
2022-04-10 19:29:53 -0500

    failed: player

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import '../constants/theme.dart';
 
@@ -24,6 +25,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.MEDIA_PLAYER);
+  bool isPlaying = false;
+
+  void playMusic() {
+    isPlaying
+        ? audioPlayer.play('https://www.youtube.com/watch?v=KCCqqPqDqSY')
+        : audioPlayer.pause();
+    setState(() => isPlaying = !isPlaying);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,18 +43,28 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.pause),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.double_arrow),
+            // onPressed: null,
+            onPressed: playMusic,
+            // onPressed: playMusic(''),
+            icon: Icon(
+              isPlaying ? Icons.pause : Icons.arrow_right,
+              color: Colors.white,
+              size: 35,
+            ),
           ),
         ],
       ),
       body: Image.network(
         'https://i.pinimg.com/564x/e6/5a/07/e65a07f5afef739312ee22b8975b21f0.jpg',
         fit: BoxFit.fitWidth,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: playMusic,
+        child: Icon(
+          isPlaying ? Icons.pause : Icons.arrow_right,
+          color: Colors.white,
+          size: 35,
+        ),
       ),
     );
   }
